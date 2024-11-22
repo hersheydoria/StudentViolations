45a2b2df516118737e7afcb53063e3b84fc30295
hersheydoria <hersyedoria@gmail.com>
2024-11-22 11:46:06 +0800

    fix: recovery token and email

--- src/stores/piniaStore.js
+++ src/stores/piniaStore.js
@@ -2,13 +2,9 @@ import { defineStore } from 'pinia'
 
 export const usePiniaStore = defineStore('piniaStore', {
   state: () => ({
-    sessionToken: null,
     recoveryToken: null
   }),
   actions: {
-    setSessionToken(token) {
-      this.sessionToken = token
-    },
     setRecoveryToken(token) {
       this.recoveryToken = token
     }
